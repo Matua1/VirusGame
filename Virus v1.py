4d6e57f0e5e4ffb5f3577d5435aab97a859cf8bc
Matua1 <70490474+Matua1@users.noreply.github.com>
2021-11-22 13:38:39 +1300

    Update Virus v1.py

--- Virus v1.py
+++ Virus v1.py
@@ -1,5 +1,13 @@
 #Purpose: Create a game that informs our school community about the virus
 
+#A Virus simulator that encourages our Pap High community to learn more about Virus'
+#Introduction to the Project
+time.sleep(.05)
+print("Welcome to the Pap High Virus Simulator\n Please follow the prompts: ")
+#A requirement of this project will be to import a non-standard python library
+import time
+
+
 #Initiate user inputs
 ctry_in = []
 num_in = []
@@ -7,15 +15,15 @@ maxLengthList = 3
 
 while len(ctry_in) < maxLengthList:
   
-    try:
        ctry = str(input("Please enter the country name: ")).upper()
+    if ctry != "":
+      print("Invalid entry, please input a name. ")
        ctry_in.append(ctry)
        num_infected = int(input("Please enter the number infected: "))
+    if num_infected != 0:
+        print("Invalid entry, please try again. ")      
        num_in.append(num_infected)
-       ctry_num = "Your country is {} and Infected numbers are {}."
-    except ValueError:
-       print("please enter a number.")
-    finally:
+     else:
        ctry_num = "Your country is {} and Infected numbers are {}"
        print(ctry_num.format(ctry_in, num_in))
 
@@ -30,22 +38,7 @@ def update():
        print(ctry_num.format(ctry_in, num_in))
        
 # this creates 5 iterations to gather data over a 5 day period.
-def week_data():
-    day = 0
-     
 
-print("Day 2:")
-week_data()
-update()
-print("Day 3:")
-week_data()
-update()
-print("Day 4:")
-week_data()
-update()
-print("Day 5:")
-week_data()
-update()
 
 for ctry in range(4):
     print(num_in, ctry_in)
